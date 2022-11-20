f64db5eab3256cf6d03d679dcd5af536b555d6b3
alstjr5949 <alstjr5949@gmail.com>
2022-11-21 00:46:41 +0900

    <study> tailwind study 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,28 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="rounded-3xl bg-white p-10 shadow-2xl"></div>
+      <div className="overflow-hidden rounded-3xl bg-white shadow-2xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-2xl text-white">Profile</span>
+        </div>
+        <div className="relative -mt-5 rounded-3xl bg-white p-6">
+          <div className="relative -top-16 flex items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 rounded-full bg-red-400" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$3,400</span>
+            </div>
+          </div>
+          <div className="relative -mt-10 flex flex-col items-center">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">New York, USA</span>
+          </div>
+        </div>
+      </div>
       <div className="rounded-3xl bg-white p-10 shadow-2xl"></div>
       <div className="rounded-3xl bg-white p-10 shadow-2xl"></div>
     </div>
