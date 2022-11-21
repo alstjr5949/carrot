a30b18ef4e4ba85ae8a75b59dfb501f3a1546af7
alstjr5949 <alstjr5949@gmail.com>
2022-11-21 20:51:45 +0900

    <study> tailwind modifier for list

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,17 @@ const Home: NextPage = () => {
     <div className="flex min-h-screen flex-col gap-10 bg-slate-400 py-20 px-10">
       <div className="rounded-3xl bg-white p-10 shadow-2xl">
         <span className="text-3xl font-semibold">Select Item</span>
-        <div className="my-2 flex justify-between">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Tooly Table</span>
-          <span className="font-semibold">$80</span>
-        </div>
+        <ul>
+          {[1, 2, 3, 4, 5].map((i) => (
+            <div
+              key={i}
+              className="my-2 flex justify-between odd:bg-blue-50 even:bg-yellow-50"
+            >
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
         <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
           <span>Total</span>
           <span className="font-semibold">$99</span>
