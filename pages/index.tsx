e8e446fb60aa199d564c68382f768fc39e9753c2
alstjr5949 <alstjr5949@gmail.com>
2022-12-02 22:57:18 +0900

    <study> peer modifier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,87 +2,21 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col gap-10 bg-slate-400 py-20 px-10">
-      <div className="rounded-3xl bg-white p-10 shadow-2xl">
-        <span className="text-3xl font-semibold">Select Item</span>
-        <ul>
-          {[1, 2, 3, 4, 5].map((i) => (
-            <div
-              key={i}
-              className="my-2 flex justify-between odd:bg-blue-50 even:bg-yellow-50"
-            >
-              <span className="text-gray-500">Grey Chair</span>
-              <span className="font-semibold">$19</span>
-            </div>
-          ))}
-        </ul>
-        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
-          <span>Total</span>
-          <span className="font-semibold">$99</span>
-        </div>
-        <button className="mx-auto mt-5 w-full rounded-xl bg-blue-500 p-3 text-center text-white hover:bg-teal-500 active:text-black">
-          Checkout
-        </button>
-      </div>
-      <div className="group overflow-hidden rounded-3xl bg-white shadow-2xl">
-        <div className="bg-blue-500 p-6 pb-14">
-          <span className="text-2xl text-white">Profile</span>
-        </div>
-        <div className="relative -mt-5 rounded-3xl bg-white p-6">
-          <div className="relative -top-16 flex items-end justify-between">
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Orders</span>
-              <span className="font-medium">340</span>
-            </div>
-            <div className="h-24 w-24 rounded-full bg-red-400 transition-colors group-hover:bg-yellow-400" />
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Spent</span>
-              <span className="font-medium">$3,400</span>
-            </div>
-          </div>
-          <div className="relative -mt-10 flex flex-col items-center">
-            <span className="text-lg font-medium">Tony Molloy</span>
-            <span className="text-sm text-gray-500">New York, USA</span>
-          </div>
-        </div>
-      </div>
-      <div className="rounded-3xl bg-white p-10 shadow-2xl">
-        <div className="mb-5 flex items-center justify-between">
-          <span>&larr;</span>
-          <div className="space-x-3">
-            <span>⭐️ 4.9</span>
-            <span className="rounded-md p-2 shadow-xl">🧡</span>
-          </div>
-        </div>
-        <div className="mx-auto mb-5 aspect-square w-96 bg-zinc-400" />
-        <div className="flex flex-col">
-          <span className="mb-1.5 text-xl font-medium">Swoon Lounge</span>
-          <span className="text-xs text-gray-500">Chair</span>
-          <div className="mt-3 mb-5 flex items-center justify-between">
-            <div className="space-x-2">
-              <button className="h-5 w-5 rounded-full bg-yellow-500 ring-yellow-500 ring-offset-2 transition focus:ring-2" />
-              <button className="h-5 w-5 rounded-full bg-indigo-500 ring-indigo-500 ring-offset-2 transition focus:ring-2" />
-              <button className="h-5 w-5 rounded-full bg-teal-500 ring-teal-500 ring-offset-2 transition focus:ring-2" />
-            </div>
-            <div className="flex items-center space-x-10">
-              <button className="flex aspect-square w-12 items-center justify-center rounded-lg bg-blue-200  p-2.5 text-xl text-gray-500">
-                -
-              </button>
-              <span>1</span>
-              <button className="flex aspect-square w-12 items-center justify-center rounded-lg bg-blue-200  p-2.5 text-xl text-gray-500">
-                +
-              </button>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-2xl font-medium">$450</span>
-            <button className="rounded-md bg-blue-500 py-2 px-12 text-white">
-              Add to Cart
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <form className="flex flex-col space-y-2 p-5">
+      <input
+        type="text"
+        required
+        placeholder="Username"
+        className="peer rounded-lg border border-gray-400 p-1"
+      />
+      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
+        This input is invalid
+      </span>
+      <span className="hidden peer-valid:block peer-valid:text-green-500">
+        Awesome Username
+      </span>
+      <input type="submit" value="Login" className="bg-white" />
+    </form>
   );
 };
 
